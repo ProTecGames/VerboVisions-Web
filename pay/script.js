03aa1667348da5047a0b5fb56c8992995c38a8e6
Prakhar Doneria <gamesprotec@gmail.com>
2024-07-04 18:21:24 +0000

    Payment Gateway

---
+++ pay/script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const uidParam = urlParams.get('uid');
+  const uidInput = document.getElementById('uid');
+
+  if (uidParam) {
+      uidInput.value = uidParam;
+      uidInput.disabled = true;
+  }
+
+  paypal.Buttons({
+      createOrder: function(data, actions) {
+          return actions.order.create({
+              purchase_units: [{
+                  amount: {
+                      value: '10'
+                  },
+                  description: 'VerboVisions -  Payment'
+              }]
+          });
+      },
+      onApprove: function(data, actions) {
+          return actions.order.capture().then(function(details) {
+              const uid = uidInput.value;
+              handleSuccess(uid);
+          });
+      },
+      onCancel: function(data) {
+          alert('Payment was canceled');
+      }
+  }).render('#paypal-button-container');
+});
+
+async function handleSuccess(uid) {
+  try {
+      const response = await fetch(`https://visionary-sliq.onrender.com/add?id=${uid}`);
+      if (response.ok) {
+          const data = await response.text();
+          console.log('Server response:', data);
+      } else {
+          alert('Failed to send payment result to server');
+      }
+  } catch (error) {
+      console.error('Error:', error);
+  }
+}
